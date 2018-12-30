d480133cc986991de0743bd80f491f6e4cc866af
BnNjJ <45629005+BnNjJ@users.noreply.github.com>
2018-12-31 01:33:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const discord = require('discord.js');
 const client = new discord.Client();
 require('events').EventEmitter.defaultMaxListeners = Infinity;
 const fs = require('fs');
-let xp = require("./xp.json");
 
 
 
 const prefix = '~';
-const ownerID = '';
+const ownerID = '482925833535619083';
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -49,34 +48,6 @@ client.on('message', message =>{
     } catch (e) {
         console.log(e.stack);
     }
-
-    let xpAdd = Math.floor(Math.random() * 7) + 8;
-    console.log(xpAdd);
-  
-    if(!xp[message.author.id]){
-      xp[message.author.id] = {
-        xp: 0,
-        level: 1
-      };
-    }
-  
-  
-    let curxp = xp[message.author.id].xp;
-    let curlvl = xp[message.author.id].level;
-    let nxtLvl = xp[message.author.id].level * 300;
-    xp[message.author.id].xp =  curxp + xpAdd;
-    if(nxtLvl <= xp[message.author.id].xp){
-      xp[message.author.id].level = curlvl + 1;
-      let lvlup = new Discord.RichEmbed()
-      .setTitle("Level Up!")
-      .setColor(purple)
-      .addField("New Level", curlvl + 1);
-  
-      message.channel.send(lvlup).then(msg => {msg.delete(5000)});
-    }
-    fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-      if(err) console.log(err)
-    });
 
 });
 
